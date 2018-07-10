0edb0eaf9719b8c3f971c5176e18faff246769de
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-10 16:07:41 +0800

    [Jeffery] Passed Practice 9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,37 @@
 package practice09;
 
 public class Klass {
+    public int number;
+    public Student leader;
+    public Klass(int number){
+        setNumber(number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student){
+        if(student.klass!=this){
+            System.out.print("It is not one of us.\n");
+        }else{
+            this.leader = student;
+        }
+    }
+
+    public Student getLeader(){
+        return this.leader;
+    }
+
+    public void appendMember(Student student){
+        student.klass = this;
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,60 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    public int id;
+    public String name;
+    public int age;
+    public Klass klass;
+
+    public Teacher(int id,String name, int age, Klass klass){
+        super(id,name,age);
+        setKlass(klass);
+    }
+
+    public Teacher(int id,String name, int age){
+        super(id,name,age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public String introduce(){
+        Person p = new Person(id,name,age);
+        String result = p.introduce() +" I am a Teacher. I teach ";
+        result += klass == null?"No Class.":klass.getDisplayName()+".";
+        return result;
+    }
+
+    public String introduceWith(Student student){
+        Person p = new Person(id,name,age);
+        String result = p.introduce() +" I am a Teacher. I ";
+        result += klass == student.klass?"teach "+student.name+".":"don't teach "+student.name+".";
+        return result;
+    }
 }
