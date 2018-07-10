6b98be3fc390704b10ebf7fa36cfbb437f390377
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-10 14:12:31 +0800

    [Jeffery] Passed Practice 4

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,34 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public String name;
+    public int age;
+    public Worker(String name, int age){
+        super(name,age);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce(){
+        Person p = new Person(name,age);
+        return p.introduce()+" I am a Worker. I have a job.";
+    }
 }
