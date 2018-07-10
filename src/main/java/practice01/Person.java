4c8e55c0f2ec92d96c7f9f1861bfbbcd3d1fce07
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-10 13:50:32 +0800

    [Jeffery] Passed Practice 1

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,31 @@
 package practice01;
 
 public class Person {
+    public String name;
+    public int age;
+
+    public Person(String name, Integer age){
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
 }
