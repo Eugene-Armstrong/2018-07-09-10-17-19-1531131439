0e2b48e4cf7e3ab9a8ed678955c5e083ffcc0fd7
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-10 15:08:11 +0800

    [Jeffery] Passed Practice 7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,22 @@
 package practice07;
 
 public class Klass {
+    public int number;
+    public Klass(int number){
+        setNumber(number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+    public String getDisplayName(){
+        String result = number == 0?"No Class.":"Class "+number;
+        return result;
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,44 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    public String name;
+    public int age;
+    public Klass klass;
+    public Student(String name, int age, Klass klass){
+        super(name,age);
+        setKlass(klass);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        Person p = new Person(name,age);
+        return p.introduce()+" I am a Student. I am at "+klass.getDisplayName()+".";
+    }
 }
